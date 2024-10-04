2ec02310006b3ece6337bb118885b3e66ff97cf9
nicorporate <nikonugroho.stuff@gmail.com>
2024-10-04 19:26:08 +0700

    adding image from gallery

--- src/screens/AddProductScreen.js
+++ src/screens/AddProductScreen.js
@@ -1,14 +1,179 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View, Text, TouchableOpacity, Image, ScrollView, StyleSheet} from 'react-native';
+import ImagePicker  from 'react-native-image-crop-picker';
+import { InputComponent } from '../../components/InputComponent';
+
 
 const AddProductScreen = () => {
-    // const openAlert = () => {
-    //   alert('You clicked the button!');
-    // };
-    return (
-        <View>
-        </View>
-    );
+  const [productData, setProductData] = useState({
+    productName: '',
+    imagePath: '',
+    category: null,
+    description: '',
+    price: null,
+    instagram: '',
+    facebook: '',
+    phoneNumber: ''
+  });
+
+  const addImage = () => {
+    ImagePicker.openPicker({
+      width: 2000,
+      height: 2000,
+      cropping: true
+    }).then(image => {
+      console.log(image)
+      setProductData({
+        ...productData,
+        imagePath: image.path
+      });
+    }).catch(errorMessage => {
+      console.log(errorMessage);
+    });
   };
+
+  const onInputChange = (type, value) => {
+    setProductData({
+      ...productData,
+      [type]: value
+    });
+  };
+
+  useEffect(() => {
+    console.log(productData);
+  }, [productData]);
+
+    return (
+        <View style={styles.mainContainer}>
+          <ScrollView contentContainerStyle={styles.scroll}>
+            <View style={styles.imageContainer}>
+              <TouchableOpacity
+                style={styles.imageButton}
+                onPress={() => addImage()}
+              >
+                <Image
+                  style={{ 
+                    width: productData.imagePath !== '' ? 200 : 50,
+                    height: productData.imagePath !== '' ? 200 : 50
+                   }}
+                  source={{uri: 
+                    productData.imagePath !== '' ? productData.imagePath :'https://uxwing.com/wp-content/themes/uxwing/download/video-photography-multimedia/upload-image-icon.png'}}
+                />
+              </TouchableOpacity>
+            </View>
+          <View style={styles.horizontalContainer}>
+            <InputComponent
+              placeholder='Product Name'
+            value={productData.productName}
+            onChangeText={(text) =>
+              onInputChange('productName', text)}
+ />
+          </View>
+          <View style={styles.horizontalContainer}>
+            <InputComponent
+              placeholder='Description'
+              value={productData.description}
+              onChangeText={(text) => onInputChange('description', text)}
+              isDescription={true}
+            />
+            <InputComponent
+              placeholder="Price"
+              value={productData.price}
+              onChangeText={(text) => onInputChange('price', text)}
+              isIcon={true}
+              name="dollar"
+              type="font-awesome"
+            />
+          </View>
+          <Text style={styles.sellerText}>Seller Contact </Text>
+          <InputComponent
+            placeholder="+821112112112"
+            value={productData.phoneNumber}
+            onChangeText={(text) => onInputChange('phoneNumber', text)}
+            isIcon={true}
+            name="whatsapp"
+            type="font-awesome"
+          />
+          <InputComponent
+            placeholder="@testInstagram"
+            value={productData.instagram}
+            onChangeText={(text) => onInputChange('instagram', text)}
+            isIcon={true}
+            name="instagram"
+            type="font-awesome"
+          />
+          <InputComponent
+            placeholder="Test Facebook"
+            value={productData.facebook}
+            onChangeText={(text) => onInputChange('facebook', text)}
+            isIcon={true}
+            name="facebook-square"
+            type="font-awesome"
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.saveButton}>
+              <Text style={styles.saveText}>SAVE</Text>
+            </TouchableOpacity>
+          </View>
+
+          </ScrollView>
+        </View>
+
+
+    );
+};
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scroll: {
+    margin: 8,
+    paddingBottom: 8,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  imageButton: {
+    width: 200,
+    height: 200,
+    borderWidth: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  sellerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 16,
+    marginLeft: 8,
+    marginBottom: 0,
+    color: 'black'
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 8
+  },
+  saveButton: {
+    marginTop: 16,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: 'mistyrose'
+  },
+  saveText: {
+    color: 'black'
+  }
+
+})
+
+
   
   export default AddProductScreen;
+
