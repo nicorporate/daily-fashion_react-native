019a0ea583db0463902a098bd46f3363494b2290
nicorporate <nikonugroho.stuff@gmail.com>
2024-10-04 18:17:36 +0700

    m1 - adding drawer navigation and image slider

---
+++ src/screens/AddProductScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const AddProductScreen = () => {
+    // const openAlert = () => {
+    //   alert('You clicked the button!');
+    // };
+    return (
+        <View>
+        </View>
+    );
+  };
+  
+  export default AddProductScreen;
