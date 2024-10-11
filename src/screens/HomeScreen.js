f317cbb5b66f60b138e715729cb9526d4e6ea3b6
nicorporate <nikonugroho.stuff@gmail.com>
2024-10-11 11:51:03 +0700

    m3 - adding image carousel

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet, FlatList, TouchableOpacity, Image} from 'react-native';
-import { categoryList } from '../data/Data';
+import React, {useState, useRef} from 'react';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity, Image, Dimensions} from 'react-native';
+import { categoryList, imageSlider } from '../data/Data';
 // import { SliderBox } from 'react-native-image-slider-box';
+import Carousel from 'react-native-snap-carousel';
 
-
+// const { width: screenWidth } = Dimensions.get('window');
+const { width: screenWidth } = Dimensions.get('window');
 
 const HomeScreen = () => {
+    const [entries, setEntries] = useState([
+        { id: '1', image: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvcHBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' },
+        { id: '2', image: 'https://media.istockphoto.com/photos/tourist-woman-with-backpack-rear-view-picture-id1212702893?b=1&k=20&m=1212702893&s=170667a&w=0&h=0H3fStHp4Cspdyyd0Mrv2P-OT-bVxSh_CPIggBFAWPA=' },
+        { id: '3', image: 'https://media.istockphoto.com/photos/portrait-of-a-man-sitting-with-a-suit-with-a-watch-studio-picture-id909869956?b=1&k=20&m=909869956&s=170667a&w=0&h=aCTsGeeDq_aK6__fWA32qPzOcXls9f2fYTEn_cJwCvg=' },
+        { id: '4', image: 'https://images.unsplash.com/photo-1598343175492-9e7dc0e63cc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2xpcHBlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' },
+        { id: '5', image: 'https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' },
+      ]);
+      const carouselRef = useRef(null);
+    
+      const renderItem = ({ item }) => {
+        return (
+          <View style={styles.slide}>
+            <Image source={{ uri: item.image }} style={styles.image} resizeMode='contain' />
+            
+          </View>
+        );
+      };
     return (
         <View style={styles.mainContainer}>
             {/* <SliderBox
@@ -16,7 +35,22 @@ const HomeScreen = () => {
             /> */}
                 <View style={styles.titleContainer}>
                     <Text style={styles.text}>
-                    Categories
+                        Overview
+                    </Text>
+                </View>
+                <Carousel
+                    ref={carouselRef}
+                    data={entries}
+                    renderItem={renderItem}
+                    sliderWidth={screenWidth}
+                    itemWidth={screenWidth * 0.9}
+                    layout={'default'}
+                    autoplay={true}
+                    loop={true}
+                />
+                <View style={styles.titleContainer}>
+                    <Text style={styles.text}>
+                        Categories
                     </Text>
                 </View>
                 <FlatList
@@ -79,7 +113,24 @@ const HomeScreen = () => {
     },
     itemName: {
         color: 'black'
-    }
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      slide: {
+        backgroundColor: 'white',
+        borderRadius: 8,
+        height: screenWidth * 0.3,
+        padding: 20,
+        justifyContent: 'center',
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 8,
+      },
 });
   
   export default HomeScreen;
